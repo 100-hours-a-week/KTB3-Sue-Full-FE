47ad523d5c277fddeaee2b2ee67ee859d71c41b5
osj1405 <osj1405@naver.com>
2025-11-19 22:50:17 +0900

    edit modal background

--- src/utils/modal.js
+++ src/utils/modal.js
@@ -1,21 +1,25 @@
 function showPostDeleteModal(){
     const modal = document.querySelector("#post-delete-modal")
     modal.classList.remove('hidden');
+    document.body.style.overflow = 'hidden'
 }
 
 function hidePostDeleteModal(){
     const modal = document.querySelector('#post-delete-modal')
     modal.classList.add('hidden')
+    document.body.style.overflow = 'auto'
 }
 
 export function showCommentDeleteModal(){
     const modal = document.querySelector("#comment-delete-modal")
     modal.classList.remove('hidden')
+    document.body.style.overflow = 'hidden'
 }
 
 export function hideCommentDeleteModal() {
     const modal = document.querySelector("#comment-delete-modal")
     modal.classList.add('hidden')
+    document.body.style.overflow = 'auto'
 }
 
 export function initPostModalEvents(){
